5d42075a72e9d75e7b01b45d9dad28da7d07c7e2
GrasslandsPublic <58302717+GrasslandsPublic@users.noreply.github.com>
2025-09-18 14:27:51 -0600

    Update ProgressUpdate.cs Nullables fixed

--- PrivacyLens/PrivacyLens/Models/ProgressUpdate.cs
+++ PrivacyLens/PrivacyLens/Models/ProgressUpdate.cs
@@ -3,11 +3,11 @@ namespace PrivacyLens.DocumentProcessing.Models
 {
     public class ProgressUpdate
     {
-        public string Phase { get; set; }
-        public string Status { get; set; }
-        public string Icon { get; set; }
-        public string Detail { get; set; }
-        public int? Percentage { get; set; }
-        public int? ChunksCreated { get; set; }
+        public string Phase { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Icon { get; set; } = "";
+        public string Detail { get; set; } = "";
+        public int? Percentage { get; set; } = "";
+        public int? ChunksCreated { get; set; } = "";
     }
 }
